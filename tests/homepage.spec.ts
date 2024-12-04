627099e5bb292874b1ef53756372ed63d0732651
Marko <markobattlenet@gmail.com>
2024-12-04 02:26:26 +0100

    first-capture

--- tests/homepage.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Portfolio homepage should load', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveTitle("Marko Avram | Portfolio");
-});
